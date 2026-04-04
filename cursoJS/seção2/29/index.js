f237c39d71d6636096fa1802f203b216cf744aba
Luiz Carlos Vazo <luizcarlosvazo89@gmail.com>
2026-04-04 17:15:07 -0300

    Atualização de curso

---
+++ cursoJS/seção2/29/index.js
@@ -0,0 +1,35 @@
+let alunos = ['Luiz', 'Maria', 'João'];
+alunos = 123;
+
+console.log(typeof alunos);
+console.log(alunos instanceof Array);
+
+
+
+
+// alunos.push('Luiza');
+// alunos.push('Eduardo');
+
+// console.log(alunos[50]);
+
+// delete alunos[1]
+// console.log(alunos);
+
+// const removidos = alunos.pop();
+// const removidos = alunos.shift();
+
+// alunos.unshift('Luiza'); // Adiciona no começo
+// alunos.unshift('Fábio');
+
+// alunos.push('Luiza'); // Adiciona no fim
+// alunos.push('Fábio');
+
+// alunos[alunos.length] = 'Luiza';
+// alunos[alunos.length] = 'Fábio';
+// alunos[alunos.length] = 'Luana';
+
+// alunos[0] = 'Eduardo';
+// alunos[3] = 'Diana';
+// console.log(alunos);
+// console.log(alunos[0]);
+// console.log(alunos[2]);
---
+++ cursoJS/seção2/31/index.js
@@ -0,0 +1,12 @@
+// function soma(x, y){
+//     const resultado = x+y;
+//     return resultado; 
+// }
+
+// console.log(soma(2, 2));
+// console.log(soma('Luiz', ' Carlos'));
+// console.log(soma(10, 5));
+
+const raiz = n => n **0.5;
+
+console.log (raiz(9));
